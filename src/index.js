3f983d7a7e3b956b7a5332cb1b65313195552867
Gregor Herdmann <10243309+gregor@users.noreply.github.com>
2024-03-13 23:27:15 +0100

    feat: Calculate image size and rows from row count (#1)

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ import {
   exportedFileQuality, 
   containerHeight, 
   containerWidth,
+  columns,
   htmlFile,
   jpegFile,
   gridGap,
-  picsToDisplay, 
   randomizePics, 
   saveCollageToFile 
   } from './config.js';
@@ -28,7 +28,7 @@ function randomizeOrder(images) {
   return images.sort(() => Math.random() - 0.5);
 }
 
-async function generateCollage(assetsFolderPath, targetCount, containerWidth, containerHeight, randomizePics, enablePuppeteer) {
+async function generateCollage(assetsFolderPath, targetColumns, containerWidth, containerHeight, randomizePics, enablePuppeteer) {
   try {
     let files = fs.readdirSync(assetsFolderPath);
     let images = [];
@@ -40,58 +40,63 @@ async function generateCollage(assetsFolderPath, targetCount, containerWidth, co
         images.push(file);
       }
     }
-  if (randomizePics) {
-    images = randomizeOrder(images);
+    if (randomizePics) {
+      images = randomizeOrder(images);
+    }
+
+    const imageSize = containerWidth / targetColumns;
+    console.log(`Calculated tile size: ${imageSize}px`)
+    const rowCount = Math.ceil(containerHeight / imageSize);
+    console.log(`Calculated number of rows: ${rowCount}`)
+    const targetCount = targetColumns * rowCount;
+    console.log(`Calculated target count: ${targetCount}`)
+
+    const repeatedImages = images.length >= targetCount ? images : repeatImages(images, targetCount);
+
+    const html = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Image Collage</title>
+        <style>
+          .grid {
+            display: grid;
+            grid-template-columns: repeat(auto-fill, ${imageSize}px);
+            grid-auto-rows: ${imageSize}px;
+            gap: ${gridGap};
+            width: ${containerWidth}px;
+            height: ${containerHeight}px;
+            margin: 0 auto;
+          }
+          .grid img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="grid">
+        ${repeatedImages.map(image => `<img src="${path.join(assetsFolderPath, image)}" alt="${image}">`).join('')}
+        </div>
+      </body>
+      </html>
+    `;
+
+    fs.writeFileSync(htmlFile, html);
+
+    if (enablePuppeteer) {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.goto(`file://${path.join(process.cwd(), htmlFile)}`); // navigate to the page
+      const element = await page.$('.grid'); // get the element
+      await element.screenshot({path: jpegFile, type: 'jpeg', quality: exportedFileQuality, omitBackground: false});    await browser.close();
+    }
+  } catch (error) {
+    console.error(`Failed to generate collage: ${error}`);
   }
-  const repeatedImages = images.length >= targetCount ? images : repeatImages(images, targetCount);
-
-  // Calculate the size of each image
-  const imageSize = Math.sqrt((containerWidth * containerHeight) / targetCount);
-
-  const html = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Image Collage</title>
-      <style>
-      .grid {
-        display: grid;
-        grid-template-columns: repeat(auto-fill, ${imageSize}px);
-        grid-auto-rows: ${imageSize}px;
-        gap: ${gridGap};
-        max-width: ${containerWidth}px;
-        max-height: ${containerHeight}px;
-        margin: 0 auto;
-      }
-        .grid img {
-          width: 100%;
-          height: 100%;
-          object-fit: cover;
-        }
-      </style>
-    </head>
-    <body>
-      <div class="grid">
-      ${repeatedImages.map(image => `<img src="${path.join(assetsFolderPath, image)}" alt="${image}">`).join('')}
-      </div>
-    </body>
-    </html>
-  `;
-
-  fs.writeFileSync(htmlFile, html);
-
-  if (enablePuppeteer) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(`file://${path.join(process.cwd(), htmlFile)}`); // navigate to the page
-    const element = await page.$('.grid'); // get the element
-    await element.screenshot({path: jpegFile, type: 'jpeg', quality: exportedFileQuality, omitBackground: false});    await browser.close();
-  }
-} catch (error) {
-  console.error(`Failed to generate collage: ${error}`);
-}
 }
 
-generateCollage(assetsFolderPath, picsToDisplay, containerWidth, containerHeight, randomizePics, saveCollageToFile);
+generateCollage(assetsFolderPath, columns, containerWidth, containerHeight, randomizePics, saveCollageToFile);
